078401a2a21ab98e67aa71280d72ac5b4838bc0a
YUKOU-s2 <79444130+YUKOU-s2@users.noreply.github.com>
2021-03-24 16:25:36 +0900

    修正依頼改善2＋

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -36,8 +36,8 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
     if @prototype.save
       redirect_to root_path
      else
