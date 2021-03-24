5bc9b04a73b74a6768f13cfea50ce7d68ec2539e
YUKOU-s2 <79444130+YUKOU-s2@users.noreply.github.com>
2021-03-24 15:57:41 +0900

    修正依頼改善2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,6 +26,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    move_to_edit
   end
 
   def destroy
@@ -37,7 +38,8 @@ class PrototypesController < ApplicationController
   def update
     prototype = Prototype.find(params[:id])
     prototype.update(prototype_params)
-    if redirect_to root_path
+    if @prototype.save
+      redirect_to root_path
      else
       render :edit
     end
@@ -48,9 +50,9 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-    redirect_to action: :index
+  def move_to_edit
+    unless user_signed_in? && current_user.id == @prototype.user_id
+    redirect_to action: :show
    end
   end
 end
